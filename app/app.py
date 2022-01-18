05fe1467d58d042aba4e451daf5f23a555e87e11
eu3neuom <manghiuc_florin@yahoo.com>
2022-01-18 03:26:29 +0200

    Integrate the authorship analysis into app

--- app/app.py
+++ app/app.py
@@ -4,12 +4,15 @@ from model.emotion_analysis import (
     analyze_existing_song,
     analyze_artist,
 )
+import pickle
+import pandas as pd
+from model.preprocessing import normalization
 
 app = Flask(__name__)
 
 
 @app.route("/sentiment", methods=["POST"])
-def predict():
+def sentiment():
     if "lyrics" in request.form.keys():
         lyrics = request.form["lyrics"]
         sentiments = analyze_new_lyrics(
@@ -44,5 +47,28 @@ def predict():
     return msg
 
 
+@app.route("/predict", methods=["POST"])
+def predict():
+    if "lyrics" in request.form.keys():
+        lyrics = normalization(request.form["lyrics"])
+        features = tfidf_vect.transform([lyrics])
+        features = pd.DataFrame(
+            features.todense(), columns=tfidf_vect.get_feature_names_out()
+        )
+        predict = model.predict(features)
+        return predict[0]
+    else:
+        print("Invalid command")
+        return None
+
+
 if __name__ == "__main__":
-    app.run(debug=False)
+    global model, tfidf_vect
+
+    with open("./app/model/random-forest.pkl", "rb") as file:
+        model = pickle.load(file)
+
+    with open("./app/model/tfidf.pkl", "rb") as file:
+        tfidf_vect = pickle.load(file)
+
+    app.run(debug=True)
